e8a2586ff59a432dc666bdd110a29acc93c2611f
totopamimi <totopamimi@gmail.com>
2017-03-11 14:56:24 +0800

    v2.2.7.201608151620

--- jeb_linux.sh
+++ jeb_linux.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Start-up script for JEB (Linux)
+# Start-up script for JEB2 (Linux)
 
-# Prefer a JDK over a JRE, which allows support for JEB native Java plugins
+# disable GTK 3 which introduce some graphical bugs
+export SWT_GTK3=0
+
+# Prefer a JDK over a JRE
 if [ -z "$JAVA_HOME" ]; then
   JAVA=`which java`
 else
   JAVA=$JAVA_HOME/bin/java
 fi
 
-# Note: Make sure your SWT build matches your Java build (32-bit or 64-bit)
-# If the Java path generation does not yield a correct result, manually set
+# Note: If the Java path generation does not yield a correct result, manually set
 # the JAVA variable (defined above) to a correct value
-
 if [ ! -f "$JAVA" ]; then
   echo "JEB requires a Java runtime environment, please install one."
   exit -1
@@ -22,21 +23,24 @@ cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd - >/dev/null
 
-JEBDIPATH=$SCRIPTDIR/bin/jebdi.jar
-$JAVA -jar $JEBDIPATH
+INSTALLER=$SCRIPTDIR/bin/jebi.jar
+$JAVA -jar $INSTALLER --di
 RETCODE=$?
 if [ $RETCODE -lt 0 ]; then
     exit $RETCODE
 fi
 
-JEBUPPATH=$SCRIPTDIR/bin/jebup.jar
-$JAVA -jar $JEBUPPATH
+$JAVA -jar $INSTALLER --up
 RETCODE=$?
 if [ $RETCODE -lt 0 ]; then
     exit $RETCODE
 fi
 
-JEBPATH=$SCRIPTDIR/bin/jeb.jar
+if [ "$1" == "-c" ]; then
+    $JAVA -jar $SCRIPTDIR/bin/cl/jeb.jar "$@"
+    exit 0
+fi
 
-# Allow the Java VM to allocate up to 2 Gb of memory
-$JAVA -Xmx2048m -XX:-UseParallelGC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar $JEBPATH "$@"
+chmod +x $SCRIPTDIR/bin/jeb
+$SCRIPTDIR/bin/jeb -data $SCRIPTDIR/bin/workspace "$@"
+exit 0
--- jeb_macos.sh
+++ jeb_macos.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Start-up script for JEB (MacOS)
+# Start-up script for JEB2 (MacOS)
 
-# Prefer a JDK over a JRE, which allows support for JEB native Java plugins
+# Prefer a JDK over a JRE
 if [ -z "$JAVA_HOME" ]; then
   JAVA=`which java`
 else
   JAVA=$JAVA_HOME/bin/java
 fi
 
-# Note: Make sure your SWT build matches your Java build (32-bit or 64-bit)
-# If the Java path generation does not yield a correct result, manually set
+# Note: If the Java path generation does not yield a correct result, manually set
 # the JAVA variable (defined above) to a correct value
-
 if [ ! -f "$JAVA" ]; then
   echo "JEB requires a Java runtime environment, please install one."
   exit -1
@@ -22,21 +20,24 @@ cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd - >/dev/null
 
-JEBDIPATH=$SCRIPTDIR/bin/jebdi.jar
-$JAVA -jar $JEBDIPATH
+INSTALLER=$SCRIPTDIR/bin/jebi.jar
+$JAVA -jar $INSTALLER --di
 RETCODE=$?
 if [ $RETCODE -lt 0 ]; then
     exit $RETCODE
 fi
 
-JEBUPPATH=$SCRIPTDIR/bin/jebup.jar
-$JAVA -jar $JEBUPPATH
+$JAVA -jar $INSTALLER --up
 RETCODE=$?
 if [ $RETCODE -lt 0 ]; then
     exit $RETCODE
 fi
 
-JEBPATH=$SCRIPTDIR/bin/jeb.jar
+if [ "$1" == "-c" ]; then
+    $JAVA -jar $SCRIPTDIR/bin/cl/jeb.jar -c "$@"
+    exit 0
+fi
 
-# Allow the Java VM to allocate up to 2 Gb of memory
-$JAVA -Xmx2048m -XX:-UseParallelGC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar $JEBPATH "$@"
+chmod +x $SCRIPTDIR/bin/jeb.app/Contents/MacOS/jeb
+$SCRIPTDIR/bin/jeb.app/Contents/MacOS/jeb -data $SCRIPTDIR/bin/jeb.app/Contents/MacOS/workspace "$@"
+exit 0
